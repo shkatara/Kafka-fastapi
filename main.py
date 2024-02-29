af7551e4d12e53a185340d150254932591eb7d22
Shubham Katara <shubham.katara@trivago.com>
2024-02-29 11:04:28 +0100

    adding status codes

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status,Response
 from uvicorn import run
 from producer import produce_message
 app = FastAPI()
 
 
 @app.get("/produce",status_code=status.HTTP_200_OK)
-def produce():
+def produce(status_code: Response):
     message = produce_message()
-    return {"message": "Sent Successful"} if message == None else {"error": "Sending Failed"}
+    if message == None:
+        status_code.status_code = status.HTTP_200_OK
+        return {"message": "Sent Successful"} 
+    else:                 
+        status_code.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+        return {"error": "Sending Failed"}
 
 if __name__ == "__main__":
    run("main:app", host="127.0.0.1", port=8000, reload=True,log_level="info")
